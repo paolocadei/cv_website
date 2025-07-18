a57c632fa29c2c00e09cde11e944e9271d6d6884
Paolo Cadei <paolo.cadei2001@gmail.com>
2025-07-18 10:54:21 +0200

    fix problem with opening page in the middle of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop'; // 👈 import it
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -13,6 +14,7 @@ import Skills from './pages/Skills';
 function App() {
   return (
     <Router>
+      <ScrollToTop /> {/* 👈 add it here, inside <Router> */}
       <div className="min-h-screen bg-gray-50">
         <Header />
         <main>
