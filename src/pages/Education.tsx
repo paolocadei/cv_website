96cd009d4a23afd9f8595b0e97fa7ea5437ab9f0
Paolo Cadei <97834265+paolocadei@users.noreply.github.com>
2025-07-18 12:07:14 +0200

    Updated Education.tsx

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -146,7 +146,7 @@ const Education = () => {
                   </li>
                   <li className="flex items-start">
                     <span className="w-2 h-2 bg-blue-500 rounded-full mt-2 mr-3 flex-shrink-0"></span>
-                    Programming for data analysis (Python, R, SQL)
+                    Programming for data analysis (Python, R)
                   </li>
                   <li className="flex items-start">
                     <span className="w-2 h-2 bg-blue-500 rounded-full mt-2 mr-3 flex-shrink-0"></span>
