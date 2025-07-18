d7d86b0a93457b7c7cbec1205d5dcf46832e0417
Paolo Cadei <97834265+paolocadei@users.noreply.github.com>
2025-07-18 12:40:02 +0200

    Updated Experience.tsx

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -144,7 +144,9 @@ const Experience = () => {
                     <ul className="space-y-2 text-gray-700 ml-4">
                       <li className="flex items-start">
                         <span className="w-2 h-2 bg-green-500 rounded-full mt-2 mr-3 flex-shrink-0"></span>
-                        Analysed complex, high-volume, high-dimensionality data from varying sources using <strong>Python and SQL </strong> .
+                        <span>
+                          Analysed complex, high-volume, high-dimensionality data from varying sources using <strong>Python</strong> and <strong>SQL</strong>.
+                        </span>
                       </li>
                       <li className="flex items-start">
                         <span className="w-2 h-2 bg-green-500 rounded-full mt-2 mr-3 flex-shrink-0"></span>
