65cd1a943dabd01e1de6543e1189dab5f28612ed
Paolo Cadei <97834265+paolocadei@users.noreply.github.com>
2025-07-19 10:06:30 +0200

    Updated Education.tsx

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -60,7 +60,7 @@ const Education = () => {
                   </div>
                   <div className="flex items-center">
                     <Award className="w-5 h-5 text-green-500 mr-2" />
-                    <span className="text-gray-700">GPA: <strong>8.6 </strong></span> (waiting for Cum Laude)
+                    <span className="text-gray-700">GPA: <strong>8.6</strong></span>
                   </div>
                 </div>
 
