67cccd320dcff3dd3dd9e030c3b244dfb302ac0b
Paolo Cadei <97834265+paolocadei@users.noreply.github.com>
2025-07-19 17:48:15 +0200

    Updated Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -46,7 +46,10 @@ const Projects = () => {
       category: "AI/ML Engineering",
       color: "green",
       githubAccess: true,
-      image: "src/images/Master_Thesis/overview.png", // Add your image path here
+      images: [
+        "src/images/Master_Thesis/overview.png",
+        "src/images/Master_Thesis/question_answering_phase.png"
+      ], 
       detailedDescription: {
         overview: "This project addresses the challenge of translating natural language questions into SQL queries on large-scale, enterprise-grade databases. It introduces a lightweight Retrieval-Augmented Generation (RAG) pipeline optimized for the Spider 2.0-Snow benchmark using schema-aware vector search, a dual-agent architecture, and minimal model usage.",
         challenges: [
