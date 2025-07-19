68f77aa422982b92ef66269936ead40f31a993d4
Paolo Cadei <97834265+paolocadei@users.noreply.github.com>
2025-07-19 18:47:46 +0200

    Updated Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -299,6 +299,7 @@ const Projects = () => {
           <div className="text-center mb-12">
             <h1 className="text-4xl font-bold text-slate-800 mb-4">Projects Portfolio</h1>
             <p className="text-xl text-gray-600">Showcasing my work</p>
+            <p className="text-xl text-gray-600"> Work In Progress</p>
           </div>
 
           {/* Access Denied Toast */}
