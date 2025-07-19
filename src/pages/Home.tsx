c476e8c355073b36360f6a296d6ffdb71a6db76a
Paolo Cadei <97834265+paolocadei@users.noreply.github.com>
2025-07-19 09:00:40 +0200

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,7 @@ const Home = () => {
               <div className="flex flex-col sm:flex-row gap-4 justify-center sm:justify-start">
                 <a 
                   href="/CV/CV_CADEIPAOLO.pdf" 
-                  download 
+                  download="Paolo_Cadei_CV.pdf" 
                   className="bg-white text-orange-600 px-8 py-3 rounded-lg font-semibold hover:bg-gray-100 transition-colors flex items-center justify-center"
                 >
                   <Download className="w-5 h-5 mr-2" />
