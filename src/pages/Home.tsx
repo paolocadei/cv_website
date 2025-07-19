0104bd10b0812b226e2fa21de642f347f8f8ebff
Paolo Cadei <97834265+paolocadei@users.noreply.github.com>
2025-07-19 09:11:09 +0200

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -211,10 +211,16 @@ const Home = () => {
               <Mail className="w-5 h-5 mr-2" />
               Get in Touch
             </button>
-            <button className="bg-transparent border-2 border-white text-white px-8 py-3 rounded-lg font-semibold hover:bg-white hover:text-slate-800 transition-colors flex items-center justify-center">x
-              <Download className="w-5 h-5 mr-2" />
-              Download Resume
-            </button>
+            <a 
+                  href="https://github.com/paolocadei/cv_website/raw/main/public/CV/Paolo_Cadei_CV.pdf" 
+                  download="Paolo_Cadei_CV.pdf" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="bg-white text-orange-600 px-8 py-3 rounded-lg font-semibold hover:bg-gray-100 transition-colors flex items-center justify-center"
+                >
+                  <Download className="w-5 h-5 mr-2" />
+                  Download CV
+                </a>
           </div>
         </div>
       </section>
