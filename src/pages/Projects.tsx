d959421538dc697a2e3af69c3efbd6dbf817005b
Paolo Cadei <97834265+paolocadei@users.noreply.github.com>
2025-07-19 16:04:48 +0200

    Updated Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
     {
       title: "CustomerDataGPT",
       description: "Developed an LLM-powered RAG model in Python to answer natural language questions about GCP tables, generate SQL queries, and provide insights. Integrated with Slack for seamless user interaction.",
-      technologies: ["Python", "Pandas", "Scikit-learn", "Tableau"],
+      technologies: ["Python", "GCP", "Slack API/APP", "RAG LLM", "Qdrant (Vector Database)"],
       year: "2024",
       category: "Machine Learning",
       color: "blue"
