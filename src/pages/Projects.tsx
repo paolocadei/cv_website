25322ff443a46bf4d7233b7832bab6ac6e0d83d5
Paolo Cadei <97834265+paolocadei@users.noreply.github.com>
2025-07-20 07:37:21 +0200

    Updated Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -49,7 +49,7 @@ const Projects = () => {
       githubLink: "https://github.com/paolocadei/Spider2.0-Thesis",
       images: [
         "CV/images/Master_Thesis/overview.png",
-        "/images/Master_Thesis/question_answering_phase.png"
+        "CV/images/Master_Thesis/question_answering_phase.png"
       ],
       detailedDescription: {
         overview: "This project addresses the challenge of translating natural language questions into SQL queries on large-scale, enterprise-grade databases. It introduces a lightweight Retrieval-Augmented Generation (RAG) pipeline optimized for the Spider 2.0-Snow benchmark using schema-aware vector search, a dual-agent architecture, and minimal model usage.",
